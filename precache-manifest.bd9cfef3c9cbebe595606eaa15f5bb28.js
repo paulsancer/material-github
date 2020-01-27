9ca43a2f45b36e5d97635e1b6301e0136bf667b5
paulsancer <paulsancer.io@gmail.com>
2020-01-26 22:33:26 -0600

    Updates

---
+++ precache-manifest.bd9cfef3c9cbebe595606eaa15f5bb28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c841443bfa11ee1f0bad4e4403d10531",
+    "url": "/material-github/index.html"
+  },
+  {
+    "revision": "939acb08724ffcafc5e8",
+    "url": "/material-github/static/css/main.6b862081.chunk.css"
+  },
+  {
+    "revision": "0c306bbaeddf3e540b00",
+    "url": "/material-github/static/js/2.2788e719.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/material-github/static/js/2.2788e719.chunk.js.LICENSE"
+  },
+  {
+    "revision": "939acb08724ffcafc5e8",
+    "url": "/material-github/static/js/main.9d5dbb2f.chunk.js"
+  },
+  {
+    "revision": "db94e7ff13c3b6e96fde",
+    "url": "/material-github/static/js/runtime-main.99d7fb4c.js"
+  },
+  {
+    "revision": "2b1c6a7a2d1a72e8ed79d30531759574",
+    "url": "/material-github/static/media/under-construction.2b1c6a7a.png"
+  }
+]);
